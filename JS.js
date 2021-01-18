48e169edf1861162774956e5a4bc827613ab257e
Ali_Taghipour <taghipourali19@gamil.com>
2021-01-18 03:37:15 +0330

    add project

---
+++ JS.js
@@ -0,0 +1,18 @@
+﻿function show(i) {
+    big_title2.innerHTML = "ماشین" + i; 
+    big_img.src = "car/0" + i + " car.png";
+    toz.innerHTML = "توضیحات بیشتر درباره ی ماشین" + "  " + i;
+}
+i = 0;
+function bast() {
+    if (i === 0) {
+        grid.style.display = "none";
+        button.innerHTML = "نمایش بیشتر";
+        i = 1;
+    }
+    else if (i === 1) {
+        grid.style.display = "block";
+        button.innerHTML = "بستن نمایش بیشتر";
+        i = 0;
+    }
+}
